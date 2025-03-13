348fe31e8c91597517c880c0f1d8d1dabbd07035
Atimscreative <timilehinsunday144@gmail.com>
2025-03-13 17:07:59 +0100

    Files update

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Index() {
+  return <></>;
+}
+
+const colors = {
+  col: "#312651",
+  cola: "#444262",
+  cols: "#83829A",
+  colx: "#ff7754",
+  cold: "#e6e4e6",
+};
