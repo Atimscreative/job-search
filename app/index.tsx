c5b58ab33fd496d825974f5061e0899ff6f1f980
Atimscreative <timilehinsunday144@gmail.com>
2025-03-20 22:58:51 +0100

    Worked on welcome screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "@/components";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+        <Stack.Screen
+          options={{
+            headerStyle: { backgroundColor: COLORS.lightWhite },
+            headerShadowVisible: true,
+            headerLeft: () => (
+              <ScreenHeaderBtn
+                iconUrl={icons.menu}
+                handlePress={() => {}}
+                dimension="60%"
+              />
+            ),
+            headerRight: () => (
+              <ScreenHeaderBtn
+                iconUrl={images.profile}
+                handlePress={() => {}}
+                dimension="100%"
+              />
+            ),
+            headerTitle: "",
+          }}
+        />
+
+        <ScrollView showsVerticalScrollIndicator>
+          <View style={{ flex: 1, padding: SIZES.medium }}>
+            <Welcome />
+            <Popularjobs />
+            <Nearbyjobs />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,62 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Image, FlatList } from "react-native";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+import { TextInput } from "react-native-gesture-handler";
+import { icons, SIZES } from "@/constants";
+import { useRouter } from "expo-router";
+
+const jobTypes = ["Full-time", "Part-time", "Contract"];
 
 const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
 
-export default Welcome
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabContainer}>
+        <FlatList
+          data={jobTypes}
+          nestedScrollEnabled={true}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
